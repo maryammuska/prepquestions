2110f098c468f0146cae4249e44c73ae5b61741c
Maryam Muska <maryammuska@ucla.edu>
2023-02-28 15:45:42 -0800

    link

--- array-duplicate/index.js
+++ array-duplicate/index.js
@@ -1,5 +1,5 @@
 
-// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
 
 
 
