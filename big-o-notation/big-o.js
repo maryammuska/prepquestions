e61dc8cfa2e48edf23e03dd1728971418b6add99
Maryam Muska <maryammuska@ucla.edu>
2023-03-17 15:47:42 -0700

    big-o

---
+++ big-o-notation/big-o.js
@@ -0,0 +1,11 @@
+/* Big O notation
+
+https://www.bigocheatsheet.com/
+
+https://www.youtube.com/watch?v=yei78sc_Bg4
+
+o(1) is constant - fastest, prints or executes right away
+o(n) is linear- when you have  for loop, it iteriates through each of the things you're looping through
+O(n ^ 2) quadratic
+
+ Level of performance (what is happening and how fast is it happening) */
